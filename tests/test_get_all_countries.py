0de77c953e833144eba8e9170ca7e2932ac348b0
imryn <noyimry@gmail.com>
2024-08-07 11:08:26 +0300

    update test_retrive_all_countries for getting data on all problematic countires if exist

--- tests/test_get_all_countries.py
+++ tests/test_get_all_countries.py
@@ -5,8 +5,10 @@ def test_retrive_all_countries(get_countries_service):
         get_countries = get_countries_service.get_all_countries()
         assert get_countries['statusCode'] == 200, f"countries was not return. look at {get_countries}"
         get_countries_response = get_countries['response']
-
+        countries_without_data = []
         for country in get_countries_response:
-            assert len(country['name']) != 0, f"the country does not have native name. {country}"
-            assert len(country['translations']) != 0, f"the country does not have translations. {country}"
-            assert len(country['region']) != 0, f"the country does not have region. {country}"
+            if len(country['name']) == 0 or len(country['translations']) == 0 or len(country['region']) == 0:
+                countries_without_data.append(country)
+
+        if len(countries_without_data)!=0:
+            raise Exception(f"missing data for one of the countries. look at {countries_without_data}")
